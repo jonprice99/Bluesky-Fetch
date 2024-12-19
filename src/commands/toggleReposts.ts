5387d89a3e88609e0aa1c21dc4a652d1f6fd2e7c
Jonathan Price <79269129+jonprice99@users.noreply.github.com>
2024-12-19 14:15:32 -0500

    Enforce setup flow in toggleReposts

--- src/commands/toggleReposts.ts
+++ src/commands/toggleReposts.ts
@@ -1,5 +1,5 @@
 import { ChatInputCommandInteraction, PermissionFlagsBits, SlashCommandBuilder } from 'discord.js';
-import { toggleReposts, fetchReposts } from '../settings'
+import { toggleReposts, fetchReposts, errorChannelId, usernameEntered, passwordEntered } from '../settings'
 
 export const data = new SlashCommandBuilder()
     .setName('togglereposts')
@@ -9,6 +9,17 @@ export const data = new SlashCommandBuilder()
 export async function execute(interaction: ChatInputCommandInteraction) {
     if (!interaction.memberPermissions?.has(PermissionFlagsBits.Administrator)) {
         return interaction.reply("You do not have permission to use this command.");
+    }
+
+    // Enforce bot setup flow
+    if (!errorChannelId) {
+        return interaction.reply("You have not yet set a channel to send bot errors to. Please use **/seterrorchannel**, then try again.");
+    }
+    if (!usernameEntered) {
+        return interaction.reply("You have not yet set your Bluesky username. Please use **/setusername** or **/setusernameandpassword**, then try again.")
+    }
+    if (!passwordEntered) {
+        return interaction.reply("You have not yet set your Bluesky app password. Please use **/setpassword** or **/setusernameandpassword**, then try again.")
     }
 
     toggleReposts();
